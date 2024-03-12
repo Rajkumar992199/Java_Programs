f2bbad692cf7100a0f7f3990861e599728e7481d
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2024-03-12 06:29:43 +0000

    Added myfirstprg

---
+++ OOP_VGEC/experiment_1/myfirstprg.java
@@ -0,0 +1,9 @@
+public class myfirstprg
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Welcome to java");
+		System.out.println("Learning java Now");
+		System.out.println("Programming is fun");
+	}
+}
