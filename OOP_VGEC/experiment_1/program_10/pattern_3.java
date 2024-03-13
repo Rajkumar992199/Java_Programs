0ad262497d68a9e203cd62a6f83c3a44147b0acd
Prajapati Rajkumar Rameshbhai <131981790+Rajkumar992199@users.noreply.github.com>
2024-03-13 18:43:40 +0000

    Added Pattern_3

---
+++ OOP_VGEC/experiment_1/program_10/pattern_3.java
@@ -0,0 +1,16 @@
+public class pattern_3 {
+    public static void main(String[] args) {
+        int k = 1;
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k);
+                k++;
+                if (k == 10) {
+                    k = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
